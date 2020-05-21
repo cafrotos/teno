10c7d1486da93553330f1bd2db46c5161c7d9c2e
cafrotos <hotanhiu.99@gmail.com>
2020-05-21 18:56:22 +0700

    add

---
+++ src/views/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+export default () => {
+  return (
+    <NavigationContainer>
+    
+    </NavigationContainer>
+  );
+}
