553a299f2ab4d1b5f94a3c87a6c2313eefd010cc
daovietanhuet <daovietanhsky@gmail.com>
2020-05-28 02:37:44 +0700

    input note

---
+++ src/utils/weather/editable.js
@@ -0,0 +1,9 @@
+let KEY = "1effc7a398ae1e31209ba251242911d9";
+let errorTitle = "Location is off";
+let errorMessage = "You need to turn location ON in order to get real-time local weather infomartion.";
+
+module.exports={
+    KEY,
+    errorTitle,
+    errorMessage
+  }
--- src/views/CreateNote/index.js
+++ src/views/CreateNote/index.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import CustomLayout from 'components/CustomLayout';
-import { Text } from '@ui-kitten/components';
+import { Text, Input, Layout } from '@ui-kitten/components';
+import { View } from 'react-native';
+import Icon from 'utils/weather-icon/weatherIcon';
+import { getLocation, getData, getWeather } from 'utils/weather';
 
 export default (props) => {
+
+  useEffect(async () => {
+    // await getWeather()
+    // let data = new getData()
+    //console.log(data)
+  })
+
   return (
-    <CustomLayout>
-      <Text>
-        Create Note
-      </Text>
+    <CustomLayout showButton={false}>
+      <View style={{ width: "100%", height: "100%", backgroundColor: "rgba(0,0,0,.25)" }}>
+        <View style={{ position: "absolute", bottom: 0, width: "100%" }}>
+          <Layout style={{flex: 1, flexDirection: 'row', padding: 5}}>
+            <Icon name="wi-day-sunny" size={40}/>
+            
+          </Layout>
+          <Input
+            placeholder="Write your emotion"
+            autoFocus={true}
+            multiline={true}
+            textStyle={{ minHeight: 100 }}
+            style={{ width: "100%", borderColor: "transparent" }}
+          >
+          </Input>
+        </View>
+      </View>
     </CustomLayout>
   )
 }
