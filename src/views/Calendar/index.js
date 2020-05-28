1fd0a172419c61e36ac9a58638c32047acdca3e1
cafrotos <hotanhiu.99@gmail.com>
2020-05-28 11:17:16 +0700

    update

--- src/views/Calendar/index.js
+++ src/views/Calendar/index.js
@@ -3,29 +3,6 @@ import CustomLayout from 'components/CustomLayout';
 import { Text, Calendar } from '@ui-kitten/components';
 import ListDiaries from 'components/ListDiaries';
 
-const RenderCalendar = ({
-  onChangeDate,
-}) => {
-  const [date, setDate] = useState(new Date());
-
-  const _onChangeDate = (_date) => {
-    setDate(_date)
-    if (typeof onChangeDate === 'function') {
-      onChangeDate(_date)
-    }
-  }
-
-  return (
-    <Calendar
-      date={date}
-      onSelect={_onChangeDate}
-      style={{
-        width: "100%"
-      }}
-    />
-  )
-}
-
 export default (props) => {
   const [date, setDate] = useState(new Date());
 
@@ -36,7 +13,13 @@ export default (props) => {
       <ListDiaries
         data={[]}
         header={
-          <RenderCalendar onChangeDate={_onChangeDate} />
+          <Calendar
+            date={date}
+            onSelect={_onChangeDate}
+            style={{
+              width: "100%"
+            }}
+          />
         }
       />
     </CustomLayout>
