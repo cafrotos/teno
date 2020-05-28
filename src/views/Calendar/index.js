bf08449948dfe87ea45ac579711ed1ed22fcfa84
cafrotos <hotanhiu.99@gmail.com>
2020-05-28 15:16:03 +0700

    update

--- src/views/Calendar/index.js
+++ src/views/Calendar/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import CustomLayout from 'components/CustomLayout';
-import { Text, Calendar } from '@ui-kitten/components';
+import { Calendar } from '@ui-kitten/components';
 import ListDiaries from 'components/ListDiaries';
 
 export default (props) => {
@@ -9,10 +9,23 @@ export default (props) => {
 
   const _onChangeDate = (_date) => setDate(_date)
 
+  const _onRefresh = async () => {
+    const notes = await NotesRepository.get();
+    const _data = data;
+    _data.push(
+      ...notes
+        .sorted('date', true)
+        .filtered(`date === ${moment(date).format("YYYY-MM-DD")}`)
+        .slice(data.length, data.length + 20)
+    )
+    setData(_data)
+  }
+
   return (
     <CustomLayout showButton={false}>
       <ListDiaries
-        data={[]}
+        data={data}
+        onRefresh={_onRefresh}
         header={
           <Calendar
             date={date}
