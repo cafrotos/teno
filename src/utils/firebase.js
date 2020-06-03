0647d369e7b7b13a5885d451fe7f981b2b795073
daovietanhuet <daovietanhsky@gmail.com>
2020-06-03 18:11:56 +0700

    firebase realtime + firestore

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,4 +1,6 @@
 import auth from '@react-native-firebase/auth';
+import database from '@react-native-firebase/database';
+import firestore from '@react-native-firebase/firestore';
 import storage from '@react-native-firebase/storage';
 import { GoogleSignin } from '@react-native-community/google-signin';
 import { LoginManager, AccessToken } from 'react-native-fbsdk';
@@ -89,7 +91,7 @@ export async function reAuthenUser() {
   });
 }
 
-export function getUserProfile() {
+export function getCurrentUserProfile() {
   var user = auth().currentUser;
   if (user != null) {
     return user
@@ -115,7 +117,7 @@ function ramdomHash(length) {
   return result;
 }
 
-export const uploadImage = (file) => new Promise((resolve, reject) => {
+export const uploadImage = (file, progressListener = (progress) => {}) => new Promise((resolve, reject) => {
   var user = auth().currentUser;
   var filename = ramdomHash(12);
   var userImageRef = storage().ref().child(`images/${user.uid}/${filename}${file.type}`);
@@ -123,6 +125,7 @@ export const uploadImage = (file) => new Promise((resolve, reject) => {
   uploadTask.on(storage.TaskEvent.STATE_CHANGED,
     (snapshot) => {
       var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      progressListener(progress)
     },
     (error) => {
       return reject(FIREBASE_STATUS.FAIL)
@@ -134,3 +137,77 @@ export const uploadImage = (file) => new Promise((resolve, reject) => {
     }
   );
 })
+
+export const uploadOneStory = (story) => {
+  var user = auth().currentUser;
+  const diariesCollection = firestore().collection("Diaries");
+  const userStoriesCollection = diariesCollection.doc(user.uid).collection("Stories");
+  return userStoriesCollection.doc(story.id).set(story, { merge: true })
+    .then(() => {
+      database().ref(`publicDiaries/${user.uid}/${story.id % 10}`).set(story); //tự động thêm vào newfeed mỗi newfeed chỉ chứa 10 stories gần nhất của user
+      return FIREBASE_STATUS.SUCCESS
+    })
+    .catch(() => {
+      return FIREBASE_STATUS.FAIL
+    })
+}
+
+export const updateOneStory = (story) => {
+  var user = auth().currentUser;
+  const diariesCollection = firestore().collection("Diaries");
+  const userStoriesCollection = diariesCollection.doc(user.uid).collection("Stories");
+  return userStoriesCollection.doc(story.id).update(story)
+    .then(() => {
+      return FIREBASE_STATUS.SUCCESS
+    })
+    .catch(() => {
+      return FIREBASE_STATUS.FAIL
+    })
+}
+
+//lasvisible là doc cuối dùng của snapshot của collection trước
+//lastVisible = querySnapshots.docs[querySnapshots.docs.length-1];
+//code lấy story theo 20 dòng một
+export const getStories = (lastVisible) => {
+  var user = auth().currentUser;
+  const diariesCollection = firestore().collection("Diaries");
+  const userStoriesCollection = diariesCollection.doc(user.uid).collection("Stories");
+  const query = userStoriesCollection
+                .orderBy("createAt")
+                .limit(20)
+                .lastVisible?startAfter(lastVisible):{} //nếu có truyền lastvisible thì chạy hàm này không thì thôi
+  return query.get()
+    .then(function(querySnapshot) {
+      return querySnapshot
+    })
+    .catch(function(error) {
+        return FIREBASE_STATUS.FAIL
+    });
+}
+
+export const getUserPublicStory = (uid, lastVisible) => {
+  var user = auth().currentUser;
+  const diariesCollection = firestore().collection("Diaries");
+  const userStoriesCollection = diariesCollection.doc(uid).collection("Stories");
+  const query = userStoriesCollection
+                .where("isPublic", "==", "true") //đã cài rule chỉ cho lấy story public của user khác trên firebase
+                .orderBy("createAt")
+                .limit(20)
+                .lastVisible?startAfter(lastVisible):{}
+  return query.get()
+    .then(function(querySnapshot) {
+      return querySnapshot
+    })
+    .catch(function(error) {
+        return FIREBASE_STATUS.FAIL
+    });
+}
+
+//code lắng nghe sự kiện realtime khi newfeed được cập nhật qua callback
+export const newsfeed = (cb) => {
+  const subscriber = database();
+  database().ref(`publicDiaries`).on("child_changed", (snapshot) => {
+    cb(snapshot)
+  })
+  return () => subscriber()
+}
