a00f8dcaa4af9fc1229ff296564e9f9d40e4e947
daovietanhuet <daovietanhsky@gmail.com>
2020-06-04 14:57:39 +0700

    newsfeed

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -3,28 +3,23 @@ import CustomLayout from 'components/CustomLayout';
 import { useNavigation } from '@react-navigation/native';
 import { STACK_NAME } from 'consts/configs';
 import ListDiaries from 'components/ListDiaries';
-import { NotesRepository } from 'repositories';
+import { getNewsfeed } from 'utils/firebase';
 
 export default (props) => {
   const navigation = useNavigation()
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    NotesRepository.create({
-      content: "HAHAHAHAHAHA"
-    })
     _onRefresh()
   }, [])
 
   const _onRefresh = async () => {
     try {
-      const notes = await NotesRepository.get();
+      const notes = await getNewsfeed(data[data.length-1]);
       const _data = data;
       _data.push(
         ...Object.values(
-          notes
-            .sorted('date', true)
-            .slice(data.length, data.length + 20)
+          notes.docs
         )
       )
       setData(_data)
